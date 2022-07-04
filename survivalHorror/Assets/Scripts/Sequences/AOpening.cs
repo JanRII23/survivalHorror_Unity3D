f04bcb7a6fa43a68475b3dd85023e2f0ce4dd561
JRII_Ataka <jrejasii@gmail.com>
2022-07-03 19:31:40 -0500

    small level design and added intro

--- survivalHorror/Assets/Scripts/Sequences/AOpening.cs
+++ survivalHorror/Assets/Scripts/Sequences/AOpening.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using StarterAssets;
+
 
 public class AOpening : MonoBehaviour
 {
+    public GameObject ThePlayer;
+    public GameObject FadeScreenIn;
+    public GameObject TextBox;
 
     void Start()
     {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(ScenePlayer());
         
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        FadeScreenIn.SetActive(false);
+        TextBox.GetComponent<Text>().text = "Eyes are on me. I need to get out of here";
+        yield return new WaitForSeconds(2);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
     }
 
 
